b8dd1631351bd5f6598c6d3f21d2919022a2040d
Mark T. Holder <mtholder@gmail.com>
2019-04-01 22:37:07 -0500

    easier data

--- homework/hw6-template.py
+++ homework/hw6-template.py
@@ -42,13 +42,14 @@ def ln_likelihood(parameter, data):
     r = parameter
     ln_l = 0.0
     try:
-        for chromosome in data:
-            _LOG.debug('Calculate ln Pr({} | r={}) HERE and add it to ln_l'.format(chromosome, r))
+        for event_type, num_bases in data:
+            msg = 'Calculate ln Pr(event={} after {} bases | r={}) HERE and add it to ln_l'
+            _LOG.debug(msg.format(event_type, num_bases, r))
     except ValueError: # we get this if we take a log 0
         return WORST_LN_L
     return ln_l
 
-def estimate_global_MLE(chromosomes):
+def estimate_global_MLE(event_list):
     min_r = 0.0
     some_guess_for_r = 0.001
     max_r = 0.5
@@ -66,29 +67,53 @@ def scipy_ln_likelihood(parameters):
     negLnL = -ln_likelihood(parameters, DATA)
     _LOG.debug('ln_likelihood({p:12.6f}) = {l}'.format(p=parameters, l=-negLnL))
     return negLnL
-    
-def get_chromosomes_from_file(data_filepath):
-    df = pd.read_excel(data_filepath)
-    series = pd.Series([2, 3], dtype='int64')
+
+def get_events_from_file(data_filepath):
+    df = pd.read_csv(data_filepath, sep='\t')
+    series = pd.Series([1], dtype='int64')
     data = []
-    current_chromosome = None
     for n, row in df.iterrows():
-        chromosome_num = row[0]
-        if not pd.isna(chromosome_num):
-            if current_chromosome:
-                data.append(current_chromosome)
-            current_chromosome = []
-        current_chromosome.append(int(row[3]))
-    if current_chromosome:
-        data.append(current_chromosome)
-    print(data)
+        data.append((row[0], int(row[1])))
     return data
+
+# DON'T CALL THIS. This is the code I used to clean the data
+# def mth_data_cleaning_get_chromosomes_from_file(data_filepath):
+
+#     df = pd.read_excel(data_filepath)
+#     series = pd.Series([2, 3], dtype='int64')
+#     data = []
+#     current_chromosome = None
+#     for n, row in df.iterrows():
+#         chromosome_num = row[0]
+#         if not pd.isna(chromosome_num):
+#             if current_chromosome:
+#                 data.append(current_chromosome)
+#             current_chromosome = []
+#         current_chromosome.append(int(row[3]))
+#     if current_chromosome:
+#         data.append(current_chromosome)
+    
+#     with open('mutt_gamete_event.tsv', 'w') as outp:
+#         header = ['event', 'num_base_pairs_to_prev']
+#         outp.write('{}\n'.format('\t'.join(header)))
+#         for c in data:
+#             num_events = len(c)
+#             prev_pos = 0
+#             for index, position in enumerate(c):
+#                 distance = str(position - prev_pos)
+#                 if index + 1 == num_events:
+#                     data_row = ['E', distance]
+#                 else:
+#                     data_row = ['R', distance]
+#                 prev_pos = position
+#                 outp.write('{}\n'.format('\t'.join(data_row)))
+#     return data
 
 def main(data_filepath):
     global DATA
-    chromosomes = get_chromosomes_from_file(data_filepath)
-    DATA = chromosomes
-    mle, lnL = estimate_global_MLE(chromosomes)
+    events = get_events_from_file(data_filepath)
+    DATA = events
+    mle, lnL = estimate_global_MLE(events)
     print('mle = {:12.6f}'.format(mle))
     print('lnL = {:12.6f}'.format(lnL))
 
